918cd9655ecc442e11334f7040dc5523f27fa811
Keeler Russell <keeler-russell@ku.edu>
2012-06-20 14:15:15 -0500

    Added subset generator.
    Particulars:
    Generates fixed-size subsets of edge indices with strong minimal change property. Such subsets have the property that the difference between one subset and the next is the replacement of one element with another. This is a Hamiltonian circuit of the subsets. The generator returns an array of 'deltas' which are structs that record which element needs to be replaced, and with what, from subset to subset. The elements are indices to an edge array: between each subset of edge indices, we need to remove one edge and add one edge, so this makes it easy to iterate over the subsets of edges of fixed size (a crucial part of the algorithm we're trying to implement).

---
+++ cpp_code/SubsetDeltaGenerator.cpp
@@ -0,0 +1,131 @@
+#include "SubsetDeltaGenerator.hpp"
+#include <cstddef>
+#include <cstring>
+
+SubsetDeltaGenerator::SubsetDeltaGenerator( unsigned int n )
+{
+	this->n = n;
+	k = 0;
+	mNumSubsetsOfLengthK = 1;
+	mCurrSubset = NULL;
+	mDeltas = NULL;
+}
+
+SubsetDeltaGenerator::~SubsetDeltaGenerator()
+{
+	if( mCurrSubset != NULL )
+	{
+		delete [] mCurrSubset;
+	}
+	if( mDeltas != NULL )
+	{
+		delete [] mDeltas;
+	}
+}
+
+unsigned long long factorial( unsigned long long x )
+{
+	if( x <= 1 )
+	{
+		return 1;
+	}
+
+	unsigned long long r = 1;
+	for( unsigned long long i = 2; i <= x; i++ )
+	{
+		r *= i;
+	}
+
+	return r;
+}
+
+unsigned long long SubsetDeltaGenerator::getNumSubsetsOfFixedLength( unsigned int k ) const
+{
+	return factorial( n ) / ( factorial( k ) * factorial( n - k ) );
+}
+
+const SubsetDelta* const SubsetDeltaGenerator::getDeltasForSubsetsOfFixedLength( unsigned int k )
+{
+	if( this->k == k )
+	{
+		return mDeltas;
+	}
+	else
+	{
+		this->k = k;
+		mNumSubsetsOfLengthK = 0;
+
+		if( mDeltas != NULL )
+		{
+			delete [] mDeltas;
+		
+		}
+		if( mCurrSubset != NULL )
+		{
+			delete [] mCurrSubset;
+		}
+
+		unsigned int numSubsets = getNumSubsetsOfFixedLength( k );
+		mDeltas = new SubsetDelta[numSubsets];
+		mCurrSubset = new unsigned int[k];
+		
+		for( int i = 0; i < k; i++ )
+		{
+			mCurrSubset[i] = i;
+		}
+
+		process( 1, 0 );
+		forward( 1, 0 );
+
+		return mDeltas;
+	}
+}
+
+void SubsetDeltaGenerator::forward( int pointer, int difference )
+{
+	if( ( pointer <  (int)k ) && ( difference - pointer < (int)n - (int)k - 1 ) )
+	{
+		forward( pointer + 2, difference + 2 );
+		process( pointer + 1, n - k + pointer + 1 );
+		reverse( pointer + 1, difference + 2 );
+		process( pointer, difference + 2 );
+		forward( pointer, difference + 1 );
+	}
+	else if( pointer == (int)k )
+	{
+		for( int i = difference + 2; i <= (int)n; i++ )
+		{
+			process( k, i );
+		}
+	}
+}
+
+void SubsetDeltaGenerator::reverse( int pointer, int difference )
+{
+	if( ( pointer < (int)k ) && ( difference - pointer < (int)n - (int)k - 1 ) )
+	{
+		reverse( pointer, difference + 1 );
+		process( pointer, difference + 1 );
+		forward( pointer + 1, difference + 2 );
+		process( pointer + 1, difference + 2 );
+		reverse( pointer + 2, difference + 2 );
+	}
+	else if( pointer == (int)k )
+	{
+		for( int i = (int)n - 1; i >= difference + 1; i-- )
+		{
+			process( k, i );
+		}
+	}
+}
+
+void SubsetDeltaGenerator::process( int posChanged, int newValue )
+{
+	SubsetDelta d;
+	d.replaceThis = mCurrSubset[posChanged - 1];
+	d.replacer = newValue;
+	mDeltas[mNumSubsetsOfLengthK++] = d;
+
+	mCurrSubset[posChanged - 1] = newValue;
+}
+
---
+++ cpp_code/SubsetDeltaGenerator.hpp
@@ -0,0 +1,34 @@
+#ifndef SUBSET_DELTA_GENERATOR_HPP
+#define SUBSET_DELTA_GENERATOR_HPP
+
+struct SubsetDelta
+{
+	unsigned int replaceThis;
+	unsigned int replacer;
+};
+
+class SubsetDeltaGenerator
+{
+	public:
+		SubsetDeltaGenerator( unsigned int n );
+		~SubsetDeltaGenerator();
+
+		unsigned long long getNumSubsetsOfFixedLength( unsigned int k ) const;
+		const SubsetDelta* const getDeltasForSubsetsOfFixedLength( unsigned int k );
+
+	private:
+		unsigned int		n;						// Num items in the set { 0, 1, ..., n - 1 }
+		unsigned int		k;						// Num items in a subset in the list of subsets
+		unsigned long long	mNumSubsetsOfLengthK;	// Number of subsets of length k calculated and added to the list
+		unsigned int		*mCurrSubset;			// Used to track state
+		// List of minimum changes needed to go from one subset to the next in the list.
+		// First item indicates what needs to be replaced, second item is what to replace it with,
+		// starting with the subset { 0, 1, ... , k - 1 }
+		SubsetDelta			*mDeltas;
+
+		void forward( int pointer, int difference );
+		void reverse( int pointer, int difference );
+		void process( int posChanged, int newValue );
+};
+
+#endif
