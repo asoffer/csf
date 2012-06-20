8b231d8593822f65963d7ec29bc8b3d07d11a8c2
Keeler Russell <keeler-russell@ku.edu>
2012-06-20 14:22:23 -0500

    Added a TreeGenerator class to generate free trees of a given order.
    Uses the algorithm for generating free trees described in:
    Wright, Richmod, Odlyzko, McKay. Constant time generation of free trees. SIAM Journal of Comuting 15 (1986), 2, pp540-548.

---
+++ cpp_code/TreeGenerator.hpp
@@ -0,0 +1,35 @@
+#ifndef TREE_GENERATOR_HPP
+#define TREE_GENERATOR_HPP
+
+#include "Tree.hpp"
+
+class TreeGenerator
+{
+	public:
+		TreeGenerator( unsigned int numVertices );
+		~TreeGenerator();
+
+		// Get the next tree generated, returns true until there are no more trees
+		// with the specified number of vertices. If false, the tree t won't necessarily
+		// contain anything meaningful, so take care using it in such a case.
+		bool nextTree( Tree & t );
+
+	private:
+		unsigned int	mNumVertices;
+		int				*L;
+		int				*currentLevelSequence;
+
+		int				p;
+		int				q;
+		int				h1;
+		int				h2;
+		int				c;
+		int				r;
+
+		bool			firstTime;
+
+		void generateFirstLevelSequence();
+		void generateNextLevelSequence();
+};
+
+#endif
