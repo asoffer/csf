fd673f2834e56698eb90f8e876833c71cd45550c
Keeler Russell <keeler-russell@ku.edu>
2012-08-21 04:43:25 -0500

    Added null pointer dereference to fatal assert.
    This allows you to get a call stack on a fatal assert, to see what conditions brought it about.

--- cpp_code/Debug.hpp
+++ cpp_code/Debug.hpp
@@ -8,19 +8,22 @@
 
 // Change to 1 to turn them on
 // Change to 0 to turn them off
-#define ASSERTIONS_ON 0
-#define DEBUG_MESSAGES_ON 1
+#define ASSERTIONS_ON 0 
+#define DEBUG_MESSAGES_ON 0
 
-/***************************************
+/*******************************************************************************
 * Assertion Macros
-***************************************/
+*
+* Note that the NULL pointer dereferences in fatalAssert() and alwaysAssert()
+* are so you can view the callstack which led to the assertion.
+*******************************************************************************/
 #if ASSERTIONS_ON
 	// Asserts the condition, and terminates the program if false.
 	#define fatalAssert( X, MSG ) if( !( X ) ) { std::cout << "Fatal assertion failed: " << std::endl << "\tContidion: " << #X << std::endl << "\tFile: " << __FILE__ << std::endl << "\tLine: " << __LINE__ << std::endl << "\tMessage: " << MSG << std::endl; int *p = NULL; cout << *p; }
 	// Asserts the condition, but does not terminate the program if false.
 	#define softAssert( X, MSG ) if( !( X ) ) { std::cout << "Soft assertion failed: " << std::endl << "\tContidion: " << #X << std::endl << "\tFile: " << __FILE__ << std::endl << "\tLine: " << __LINE__ << std::endl << "\tMessage: " << MSG << std::endl; }
 	// A fatal assert that always occurs if executed.
-	#define alwaysAssert( MSG ) { std::cout << "Always assert here: " << std::endl << "\tFile: " << __FILE__ << std::endl << "\tLine: " << __LINE__ << std::endl << "\tMessage: " << MSG << std::endl; exit( -1 ); }
+	#define alwaysAssert( MSG ) { std::cout << "Always assert here: " << std::endl << "\tFile: " << __FILE__ << std::endl << "\tLine: " << __LINE__ << std::endl << "\tMessage: " << MSG << std::endl; int *p = NULL; cout << *p; }
 #else
 	#define fatalAssert( X, MSG ) {}
 	#define softAssert( X, MSG ) {}
